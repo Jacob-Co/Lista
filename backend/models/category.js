d742fd4a38cadc67118e7e10a8c45b262ebc4651
Jacobtco <jacobtco@gmail.com>
2020-09-28 17:38:33 +0800

    Created category mongoose model

---
+++ backend/models/category.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    unique: true,
+  },
+  summary: String,
+  tasks: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Task',
+    },
+  ],
+});
+
+categorySchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+categorySchema.plugin(uniqueValidator);
+
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category;
