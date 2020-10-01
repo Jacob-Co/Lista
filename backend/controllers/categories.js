6f0aa2e707e7462dfb3f8873a256d122cf4d4fa1
Jacobtco <jacobtco@gmail.com>
2020-10-01 10:52:33 +0800

    Added patch for name and summary of Category, also get id for Category

--- backend/controllers/categories.js
+++ backend/controllers/categories.js
@@ -3,12 +3,36 @@ const Category = require('../models/category');
 
 categoryRouter.get('/', async (req, res) => {
   const categories = await Category.find({})
-  res.json(categories);
+  res.status(201).json(categories);
 });
+
+categoryRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const returnedCategory = await Category.findById(id);
+  res.status(201).json(returnedCategory);
+})
 
 categoryRouter.post('/', async (req, res) => {
   const { body } = req;
   const newCategory = new Category({ ...body });
-  const returnedCategory = newCategory.save();
-  res.json(returnedCategory);
+  const returnedCategory = await newCategory.save();
+  res.status(201).json(returnedCategory);
 })
+
+categoryRouter.patch('/name/:id', async (req, res) => {
+  const filter = { _id: req.params.id }
+  const modify = { name: req.body.name }
+  const options = { new: true }
+  const returnedCategory = await Category.findOneAndUpdate(filter, modify, options);
+  res.status(201).json(returnedCategory);
+})
+
+categoryRouter.patch('/summary/:id', async (req, res) => {
+  const filter = { _id: req.params.id }
+  const modify = { summary: req.body.summary }
+  const options = { new: true }
+  const returnedCategory = await Category.findOneAndUpdate(filter, modify, options);
+  res.status(201).json(returnedCategory);
+})
+
+module.exports = categoryRouter
