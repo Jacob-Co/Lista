371c654af830824f939b28b53d36a9010de84ca6
Jacobtco <jacobtco@gmail.com>
2020-10-03 18:24:01 +0800

    Created task controller with post to create a new task and get all tasks

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,12 +1,20 @@
 const taskRouter = require('express').Router();
 const Task = require('../models/task');
+const Category = require('../models/category');
 
-// taskRouter.post('/', async (req, res) => {
-//   const { body } = req;
-//   const newTask = new Task({ ...body });
-//   const returnedTask = await newTask.save();
-//   res.status(201).json(returnedTask);
-// })
+taskRouter.post('/', async (req, res) => {
+  const { name, content, category, endDate, recurring } = req.body;
+  const newTask = new Task({ name, content, category, endDate, recurring });
+  const returnedTask = await newTask.save();
+
+  if (category) {
+    const categoryToUpdate = await Category.findById(category);
+    categoryToUpdate.tasks = categoryToUpdate.tasks.concat(returnedTask.id);
+    await categoryToUpdate.save();
+  }
+
+  res.status(201).json(returnedTask);
+})
 
 taskRouter.get('/', async (req, res) => {
   const allTasks = await Task.find({});
@@ -16,6 +24,14 @@ taskRouter.get('/', async (req, res) => {
 // taskRouter.patch('/endDate/:id', async (req, res) => {
 //   const filter = { _id: req.params.id }
 //   const modifier = { endDate: req.body.endDate }
+//   const options = { new: true }
+//   const updatedTask = await Task.findOneAndUpdate(filter, modifier, options)
+//   res.status(202).json(updatedTask)
+// })
+
+// taskRouter.patch('/recurring/:id', async(req, res) => {
+//   const filter = { _id: req.params.id }
+//   const modifier = { recurring: req.body.recurring }
 //   const options = { new: true }
 //   const updatedTask = await Task.findOneAndUpdate(filter, modifier, options)
 //   res.status(202).json(updatedTask)
