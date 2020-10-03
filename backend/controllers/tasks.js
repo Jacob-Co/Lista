294caf7c5875fe634e387006840c5dd985e76bd7
Jacobtco <jacobtco@gmail.com>
2020-10-03 18:40:01 +0800

    Added patch for endDate for tasks

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -21,13 +21,13 @@ taskRouter.get('/', async (req, res) => {
   res.status(200).json(allTasks)
 })
 
-// taskRouter.patch('/endDate/:id', async (req, res) => {
-//   const filter = { _id: req.params.id }
-//   const modifier = { endDate: req.body.endDate }
-//   const options = { new: true }
-//   const updatedTask = await Task.findOneAndUpdate(filter, modifier, options)
-//   res.status(202).json(updatedTask)
-// })
+taskRouter.patch('/endDate/:id', async (req, res) => {
+  const filter = { _id: req.params.id }
+  const modifier = { endDate: req.body.endDate }
+  const options = { new: true }
+  const updatedTask = await Task.findOneAndUpdate(filter, modifier, options)
+  res.status(202).json(updatedTask)
+})
 
 // taskRouter.patch('/recurring/:id', async(req, res) => {
 //   const filter = { _id: req.params.id }
