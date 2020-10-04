ac561f3cc1f153ac8e722b34f80a7c707981f863
Jacobtco <jacobtco@gmail.com>
2020-10-04 21:09:40 +0800

    Commented out patch task for updateNote controller

--- backend/controllers/updateNotes.js
+++ backend/controllers/updateNotes.js
@@ -1,6 +1,7 @@
 const updateNoteRouter = require('express').Router()
 const UpdateNote = require('../models/updateNote')
 const Task = require('../models/task')
+const updateNote = require('../models/updateNote')
 
 updateNoteRouter.post('/', async (req, res) => {
   const { body } = req
@@ -36,5 +37,36 @@ updateNoteRouter.get('/', async (req, res) => {
   const allNotes = await UpdateNote.find({})
   res.status(200).json(allNotes);
 })
+
+updateNoteRouter.patch('/content/id', async (req, res) => {
+  const filter = { id: req.params.id }
+  const modifier = { content: req.body.content }
+  const options = { new: true }
+  const modUpdateNote = await UpdateNote.findByIdAndUpdate(filter, modifier, options)
+  res.send(203).json(modUpdateNote)
+})
+
+// updateNoteRouter.patch('/task/id', async (req, res) => {
+//   const updateNoteToUpdate = await UpdateNote.findById(req.params.id)
+//   const { body } = req
+
+//   if (updateNoteToUpdate.task) {
+//     if (updateNoteToUpdate.task.toString() === body.task) res.sendStatus(406).end()
+
+//     const prevTask = await Task.findById(updateNoteToUpdate.task)
+//     prevTask.updateNotes = prevTask.updateNotes.filter(updateNote => {
+//       return updateNote.toString() != updateNoteToUpdate.id
+//     })
+//     await prevTask.save()
+//   }
+
+//   const newTask = await Task.findById(body.task)
+//   newTask.updateNotes = newTask.updateNotes.concat(req.params.id)
+//   await newTask.save()
+
+//   updateNoteToUpdate.task = body.task
+//   const returnedUpdateNote = updateNoteToUpdate.save()
+//   res.send(203).json(returnedUpdateNote)
+// })
 
 module.exports = updateNoteRouter
