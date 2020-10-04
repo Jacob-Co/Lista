bc17258166d888b9ec2feee6d3b88678b1b6241a
Jacobtco <jacobtco@gmail.com>
2020-10-04 19:07:13 +0800

    Modified post new task to update the task if the req has a given task id

--- backend/controllers/updateNotes.js
+++ backend/controllers/updateNotes.js
@@ -1,8 +1,16 @@
 const updateNoteRouter = require('express').Router()
-const UpdateNote = require('../models/updateNote');
+const UpdateNote = require('../models/updateNote')
+const Task = require('../models/task')
 
 updateNoteRouter.post('/', async (req, res) => {
   const { body } = req
+
+  if (body.task) {
+    const task = await Task.findById(body.task)
+    task.updateNotes = task.updateNotes.concat(body.task)
+    await task.save()
+  }
+
   const newNote = new UpdateNote({ ...body });
   const returnedNote = await newNote.save();
   res.status(201).json(returnedNote)
