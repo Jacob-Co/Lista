3a8325f4cca1a160460554c4a10eb4ea8ee1466f
Jacobtco <jacobtco@gmail.com>
2020-10-04 09:44:05 +0800

    Added a patch cateogry for tasks controller

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -29,12 +29,79 @@ taskRouter.patch('/endDate/:id', async (req, res) => {
   res.status(202).json(updatedTask)
 })
 
-taskRouter.patch('/recurring/:id', async(req, res) => {
+taskRouter.patch('/recurring/:id', async (req, res) => {
   const filter = { _id: req.params.id }
   const modifier = { recurring: req.body.recurring }
   const options = { new: true }
   const updatedTask = await Task.findOneAndUpdate(filter, modifier, options)
   res.status(202).json(updatedTask)
 })
+
+taskRouter.patch('/name/:id', async (req, res) => {
+  const filter = { _id: req.params.id }
+  const modifier = { name: req.body.name }
+  const options = { new: true }
+  const updatedTask = await Task.findOneAndUpdate(filter, modifier, options)
+  res.status(202).json(updatedTask)
+})
+
+taskRouter.patch('/content/:id', async (req, res) => {
+  const filter = { _id: req.params.id }
+  const modifier = { content: req.body.content }
+  const options = { new: true }
+  const updatedTask = await Task.findOneAndUpdate(filter, modifier, options)
+  res.status(202).json(updatedTask)
+})
+
+taskRouter.patch('/accomplished/:id', async (req, res) => {
+  const filter = { _id: req.params.id }
+  const modifier = { accomplished: req.body.accomplished }
+  const options = { new: true }
+  const updatedTask = await Task.findOneAndUpdate(filter, modifier, options)
+  res.status(202).json(updatedTask)
+})
+
+taskRouter.patch('/overdue/:id', async (req, res) => {
+  const filter = { _id: req.params.id }
+  const modifier = { overdue: req.body.overdue }
+  const options = { new: true }
+  const updatedTask = await Task.findOneAndUpdate(filter, modifier, options)
+  res.status(202).json(updatedTask)
+})
+
+taskRouter.patch('/category/:id', async (req, res) => {
+  const taskToUpdate = await Task.findById(req.params.id)
+  const newCategoryId = req.body.category;
+
+  // check if there is a prev category
+  if (taskToUpdate.category) {
+    // return a 406 status if assigning to the same category
+    if (taskToUpdate.category.toString() === newCategoryId) res.status(406).end();
+
+    const prevCategory = await Category.findById(taskToUpdate.category);
+    prevCategory.tasks = prevCategory.tasks
+      .filter(taskId => taskId.toString() != taskToUpdate.id)
+    await prevCategory.save();
+  }
+
+  // place task in new cateogry
+  const newCategory = await Category.findById(newCategoryId);
+  newCategory.tasks = newCategory.tasks.concat(taskToUpdate.id);
+  await newCategory.save();
+
+  taskToUpdate.category = newCategoryId;
+  const updatedTask = await taskToUpdate.save()
+
+  res.status(202).json(updatedTask)
+})
+
+// taskRouter.delete('/:id', async (req, res) => {
+//   // const taskToDelete = await Task.findById(req.params.id);
+//   // if (taskToDelete.category) {
+
+//   // }
+//   await Task.findByIdAndRemove(req.params.id);
+//   res.sendStatus(204).end()
+// })
 
 module.exports = taskRouter
