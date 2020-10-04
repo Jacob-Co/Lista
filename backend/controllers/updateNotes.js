4c4417d3bc0d8bb3d5080ff0621fcfe30d2ae516
Jacobtco <jacobtco@gmail.com>
2020-10-04 20:52:55 +0800

    Added a delete updateNote to updateNote controller, also fixed post updateNote, where updateNote id is now being sent to the task instead of the task id

--- backend/controllers/updateNotes.js
+++ backend/controllers/updateNotes.js
@@ -5,15 +5,31 @@ const Task = require('../models/task')
 updateNoteRouter.post('/', async (req, res) => {
   const { body } = req
 
+  const newNote = new UpdateNote({ ...body });
+  const returnedNote = await newNote.save();
+
   if (body.task) {
     const task = await Task.findById(body.task)
-    task.updateNotes = task.updateNotes.concat(body.task)
+    task.updateNotes = task.updateNotes.concat(returnedNote.id)
     await task.save()
   }
 
-  const newNote = new UpdateNote({ ...body });
-  const returnedNote = await newNote.save();
   res.status(201).json(returnedNote)
+})
+
+updateNoteRouter.delete('/:id', async (req, res) => {
+  const updateNoteToDelete = await UpdateNote.findById(req.params.id)
+
+  if (updateNoteToDelete.task) {
+    const task = await Task.findById(updateNoteToDelete.task)
+    task.updateNotes = task.updateNotes.filter(updateNote => {
+      return updateNote.toString() != updateNoteToDelete.id
+    });
+    await task.save()
+  }
+
+  const deletedUpdateNote = await UpdateNote.findByIdAndRemove(req.params.id)
+  res.sendStatus(204).end();
 })
 
 updateNoteRouter.get('/', async (req, res) => {
