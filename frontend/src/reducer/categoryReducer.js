aee5c87e56b4600fb3727d7233487beaf5f3e500
Jacobtco <jacobtco@gmail.com>
2020-10-08 14:18:54 +0800

    Created a category reducer

---
+++ frontend/src/reducer/categoryReducer.js
@@ -0,0 +1,26 @@
+import categories from '../services/categories'
+
+export const initializeCategories = () => {
+  return async (dispatch) => {
+    const allCategories = await categories.getAll()
+    const allCategoriesHash = {}
+    allCategories.forEach(category => {
+      allCategories[category.id] = category
+    })
+    dispatch({
+      type: 'INIT_CATEGORIES',
+      data: allCategoriesHash
+    })
+  }
+}
+
+const categoryReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'INIT_CATEGORIES':
+      return action.data
+    default:
+      return state
+  }
+}
+
+export default categoryReducer
