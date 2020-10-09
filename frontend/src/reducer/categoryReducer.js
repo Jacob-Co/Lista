8a85016e1a39970cde6d1ffb0cd3ee53726aab1d
Jacobtco <jacobtco@gmail.com>
2020-10-09 18:16:25 +0800

    initializeCategories now returns an array of categories rather than an object

--- frontend/src/reducer/categoryReducer.js
+++ frontend/src/reducer/categoryReducer.js
@@ -3,18 +3,14 @@ import categories from '../services/categories'
 export const initializeCategories = () => {
   return async (dispatch) => {
     const allCategories = await categories.getAll()
-    const allCategoriesHash = {}
-    allCategories.forEach(category => {
-      allCategoriesHash[category.id] = category
-    })
     dispatch({
       type: 'INIT_CATEGORIES',
-      data: allCategoriesHash
+      data: allCategories
     })
   }
 }
 
-const categoryReducer = (state = {}, action) => {
+const categoryReducer = (state = [], action) => {
   switch (action.type) {
     case 'INIT_CATEGORIES':
       return action.data
