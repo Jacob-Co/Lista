37d8c44d78d77e63a10444b6dfe28c6b873a6136
Jacobtco <jacobtco@gmail.com>
2020-10-09 18:12:10 +0800

    Changed sendStatus to status so that json error message can be sent

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,7 +1,7 @@
 const errorHandler = (e, req, res, next) => {
   console.log(e.message)
   
-  res.sendStatus(400).json({ "error": e.message })
+  res.status(400).json({ "error": e.message })
 
   next(e);
 };
