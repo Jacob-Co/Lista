bf1976f532150db08d3fa6a591667d5f5fbeacdd
Jacobtco <jacobtco@gmail.com>
2020-10-11 16:09:02 +0800

    Created a removeCateogry action reducer

--- frontend/src/reducer/categoryReducer.js
+++ frontend/src/reducer/categoryReducer.js
@@ -20,12 +20,24 @@ export const createNewCategory = (category) => {
   }
 }
 
+export const removeCategory = (id) => {
+  return async (dispatch) => {
+    await categories.deleteCategory(id);
+    dispatch({
+      type: 'REMOVE_CATEGORY',
+      id,
+    })
+  }
+}
+
 const categoryReducer = (state = [], action) => {
   switch (action.type) {
     case 'INIT_CATEGORIES':
       return action.data
     case 'NEW_CATEGORY':
       return state.concat(action.data)
+    case 'REMOVE_CATEGORY':
+      return state.filter(category => category.id !== action.id);
     default:
       return state
   }
