89484f7dd0606b54e00b541d8fe4b3ef9c7dd0ef
Jacobtco <jacobtco@gmail.com>
2020-10-11 16:16:39 +0800

    Added a delete button for categories

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from "styled-components";
 
-import { initializeCategories } from '../reducer/categoryReducer'
+import { initializeCategories, removeCategory } from '../reducer/categoryReducer'
 import CategoryForm from './CategoryForm'
 
 const TaskDiv = styled.div`
@@ -17,13 +17,22 @@ const CategoryList = () => {
     dispatch(initializeCategories())
   }, [dispatch])
 
+  const deleteCategory = ({name, id}) => {
+    if (window.confirm(`Do you really want to delete category ${name}?`)) {
+      dispatch(removeCategory(id))
+    }
+  }
+
   return (
     <div>
       <h2>Categories:</h2>
       <CategoryForm />
       {categoryList.map(category => {
         return <div className="category" key={category.id}>
-          <p>{category.name}</p>
+          <div>
+            {category.name}----
+            <button onClick={() => {deleteCategory(category)}}>X</button>
+          </div>
           <TaskDiv className="tasks">
             {category.tasks.map(task => <p key={task.id}>> {task.name}</p>)}
           </TaskDiv>
