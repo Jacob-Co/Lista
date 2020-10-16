82af106c7f7db115470bc112e4f768b0c7e37e7e
Jacobtco <jacobtco@gmail.com>
2020-10-16 11:39:43 +0800

    Used flex box to divide the main page into two

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import styled from "styled-components";
 
 import CategoryList from './components/CategoryList'
 
+const TwoColumn = styled.div`
+  display: flex;
+  height: 100%;
+`
+
+const LeftColumn = styled.div`
+  width: 30%;
+  box-sizing: border-box;
+`
+
+const RightColumn = styled.div`
+  width: 70%;
+  box-sizing: border-box;
+  border-left: 2px solid;
+`
+
 function App() {
   return (
-    <div>
-      <CategoryList />
-    </div>
+    <TwoColumn>
+      <LeftColumn>
+        <CategoryList />
+      </LeftColumn>
+      <RightColumn>
+        Right
+      </RightColumn>
+    </TwoColumn>
   );
 }
 
