507e0cbbc942f3532712d030149760e27874cd8c
Jacobtco <jacobtco@gmail.com>
2020-10-17 14:37:10 +0800

    Renamed return object from date to UNIX

--- backend/controllers/dates.js
+++ backend/controllers/dates.js
@@ -1,8 +1,8 @@
 const dateRouter = require("express").Router();
 
 dateRouter.get("/", (req, res) => {
-  const date = Date.now();
-  res.status(200).json({ date })
+  const UNIX = Date.now();
+  res.status(200).json({ UNIX })
 })
 
 module.exports = dateRouter;
