ef1a4015f93c485e2dad19e5571c0aef41997ec9
Jacobtco <jacobtco@gmail.com>
2020-10-19 13:50:32 +0800

    Added styled comp to clock

--- frontend/src/components/HomeClock.js
+++ frontend/src/components/HomeClock.js
@@ -4,8 +4,22 @@ import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { newDate, advanceUNIX } from '../reducer/dateReducer';
 
+const ClockContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
 
-function useInterval(callback, delay) {
+const Clock = styled.div`
+  font-size: 1.3em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const useInterval = (callback, delay) => {
   const savedCallback = useRef();
 
   // Remember the latest callback.
@@ -43,14 +57,14 @@ const HomeClock = () => {
     const newDateObj = new Date(unix)
     const dateArr = newDateObj.toString().split(" ");
     const dayInfo = `${dateArr[0]} ${dateArr[1]} ${dateArr[2]}, ${dateArr[3]}`
-    const timeZone = dateArr.slice(6);
+    const timeZone = dateArr.slice(6).join(" ");
     const time = dateArr[4];
     // console.log(timeZone)
-    return (<div>
+    return (<Clock>
       <h3>{dayInfo}</h3>
       <h1>{time}</h1>
       <h3>{timeZone}</h3>
-    </div>)
+    </Clock>)
   }
 
   useEffect(() => {
@@ -65,9 +79,9 @@ const HomeClock = () => {
   }, [dispatch])
 
   return (
-    <div>
+    <ClockContainer>
       {formatClock(date.UNIX)}
-    </div>
+    </ClockContainer>
   )
 }
 
