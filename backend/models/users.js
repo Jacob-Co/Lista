8b6859c6001d227e6c7fea11d3d3769c66e1217a
Jacobtco <jacobtco@gmail.com>
2020-10-19 15:14:26 +0800

    Created new user model

---
+++ backend/models/users.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 4,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  cateogries: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category'
+  }],
+  tasks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task',
+  }],
+  updateNotes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'UpdateNote',
+  }]
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+
+userSchema.plugin(uniqueValidator);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
