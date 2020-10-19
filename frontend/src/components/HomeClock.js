9b68684a0d125c42b20527dea295a65ace8ea704
Jacobtco <jacobtco@gmail.com>
2020-10-19 13:38:19 +0800

    Formated clock, added onfocus updated on time

--- frontend/src/components/HomeClock.js
+++ frontend/src/components/HomeClock.js
@@ -1,22 +1,72 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { setDate } from '../reducer/dateReucer';
+import styled from 'styled-components';
+import { newDate, advanceUNIX } from '../reducer/dateReducer';
+
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 const HomeClock = () => {
   const dispatch = useDispatch()
-  const dateUNIX = useSelector(state => state.date);
-  const date = new Date(dateUNIX).toString();
+  const date = useSelector(state => state.date);
+
+  useInterval(() => {
+    dispatch(advanceUNIX(date.UNIX));
+    // console.log(date.original.getDay() !== new Date(date.UNIX).getDay())
+    // console.log(date.original.getDay())
+    // console.log(new Date(date.UNIX).getDay())
+    if (date.original.getDay() !== new Date(date.UNIX).getDay()) {
+      window.location.reload();
+    }
+  }, 1000)
+
+  function formatClock(unix) {
+    const newDateObj = new Date(unix)
+    const dateArr = newDateObj.toString().split(" ");
+    const dayInfo = `${dateArr[0]} ${dateArr[1]} ${dateArr[2]}, ${dateArr[3]}`
+    const timeZone = dateArr.slice(6);
+    const time = dateArr[4];
+    // console.log(timeZone)
+    return (<div>
+      <h3>{dayInfo}</h3>
+      <h1>{time}</h1>
+      <h3>{timeZone}</h3>
+    </div>)
+  }
 
   useEffect(() => {
-    dispatch(setDate());
+    dispatch(newDate());
+  }, [dispatch])
+
+  useEffect(() => {
+    window.onfocus = function() {
+      dispatch(newDate());
+      console.log(`Time updated`)
+    }
   }, [dispatch])
 
   return (
     <div>
-      1/1/2020
-      00:00:00 <br/>
-      {date}
+      {formatClock(date.UNIX)}
     </div>
   )
 }
