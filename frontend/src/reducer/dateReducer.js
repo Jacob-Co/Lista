643ea81624d389198b0e24c86cd0f11d13a99a8a
Jacobtco <jacobtco@gmail.com>
2020-10-19 13:36:57 +0800

    Fixed typo

---
+++ frontend/src/reducer/dateReducer.js
@@ -0,0 +1,31 @@
+import dates from '../services/dates'
+
+export const newDate = () => {
+  return async (dispatch) => {
+    const {UNIX} = await dates.getDate();
+    dispatch({
+      type: "SET_DATE",
+      data: {UNIX, original: new Date(UNIX)}
+    })
+  }
+}
+
+export const advanceUNIX = (UNIX) => {
+  return {
+    type: "ADVANCE",
+    data: UNIX += 1000
+  }
+}
+
+const dateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "SET_DATE":
+      return action.data;
+    case "ADVANCE":
+      return {...state, UNIX: action.data}
+    default:
+      return state
+  }
+}
+
+export default dateReducer;
