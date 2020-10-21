01536b098e452cd37dd6739540e3b45fd41fc9fb
Jacobtco <jacobtco@gmail.com>
2020-10-21 21:54:52 +0800

    Initialized FriendsList comp

---
+++ frontend/src/components/FriendsList.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+import { initializeFriends } from '../reducer/friendReducer';
+
+const FriendListDiv = styled.div`
+  position: absoulte;
+  bottom: 1rem;
+  right: 1rem;
+`
+
+const FriendsList = () => {
+  const dispatch = useDispatch();
+  const friends = useSelector(state => state.friends);
+
+  useEffect(() => {
+    useDispatch(initializeFriends());
+  }, [dispatch])
+
+  return (
+    <FriendListDiv>
+      {friends.map(friend => <div id={friend.id}>{friend.username}</div>)}
+    </FriendListDiv>
+  )
+}
+
+export default FriendsList;
