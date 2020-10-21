23d37a77b8e44cad72d2b748c0b2efc7a9cc7bc0
Jacobtco <jacobtco@gmail.com>
2020-10-21 17:24:18 +0800

    Added a login comp

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../reducer/tokenReducer'
+import { useHistory } from 'react-router-dom'
+
+const Login = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+    const credentials = { username, password };
+    dispatch(login(credentials));
+    history.push('/')
+  }
+
+  return (
+    <div>
+      <h1>Login to Ticket Book</h1>
+      <form>
+        <div onSubmit={}>
+          Username: 
+          <input
+            type="text"
+            name="username"
+            autoComplete="username"
+          />
+        </div>
+        <div>
+          Password:
+          <input
+            type="password"
+            name="password"
+            autoComplete="password"
+          />
+        </div>
+        <button>Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
