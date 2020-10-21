fb8c455a874f407822a8c5bf72421a59dd299e8d
Jacobtco <jacobtco@gmail.com>
2020-10-21 18:09:37 +0800

    Created a logout component

---
+++ frontend/src/components/Logout.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+
+import { logout } from '../reducer/tokenReducer'
+
+const LogoutDiv = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+`
+
+const Logout = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const user = useSelector(state => state.token);
+  const handleLogout = () => {
+    dispatch(logout());
+    history.push('/');
+  }
+
+  return (
+    <LogoutDiv>
+      {`Greetings ${user.username}!    `}
+      <button onClick={handleLogout}>Logout</button>
+    </LogoutDiv>
+  )
+}
+
+export default Logout
