384b2f70fa97bf80a282df331c9b244b40f07d75
Jacobtco <jacobtco@gmail.com>
2020-10-24 18:26:22 +0800

    Added a handleDragEnd handler for DragDropContext

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -2,10 +2,9 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
-import { initializeCategories, removeCategory } from '../reducer/categoryReducer'
+import { initializeCategories, removeCategory, switchIndexes } from '../reducer/categoryReducer'
 import CategoryForm from './CategoryForm'
 import CategorySide from './CategorySide'
-
 
 const CategoryList = () => {
   const dispatch = useDispatch()
@@ -21,11 +20,20 @@ const CategoryList = () => {
     }
   }
 
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+    if (result.destination.index === result.source.index) return;
+    let sourceIdx = parseInt(result.source.index)
+    let destIdx = parseInt(result.destination.index)
+    console.log(`source: ${sourceIdx}, dest: ${destIdx}`)
+    dispatch(switchIndexes(categoryList[sourceIdx], categoryList[destIdx], categoryList));
+  }
+
   return (
     <div>
       <h2>Categories:</h2>
       <CategoryForm />
-      <DragDropContext>
+      <DragDropContext onDragEnd={handleDragEnd}>
         <Droppable droppableId={'1'}>
           {provided => (
             <div
