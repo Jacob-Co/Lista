502a50f033868d3a6a236ef8dd73df3a35a37c27
Jacobtco <jacobtco@gmail.com>
2020-10-26 22:50:38 +0800

    Deleted Link, using history.push('/friends/categories':

--- frontend/src/components/FriendsList.js
+++ frontend/src/components/FriendsList.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 import { initializeFriends } from '../reducer/friendReducer';
 import { initializeFriendCategories } from '../reducer/friendCategoryReducer';
@@ -14,14 +14,16 @@ const FriendListDiv = styled.div`
 
 const FriendsList = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const friends = useSelector(state => state.friends);
 
   useEffect(() => {
     dispatch(initializeFriends());
   }, [dispatch])
 
-  const clickView = (id) => {
-    dispatch(initializeFriendCategories(id));
+  const clickView = async (id) => {
+    await dispatch(initializeFriendCategories(id));
+    history.push('/friend/categories');
   }
 
   return (
@@ -29,11 +31,9 @@ const FriendsList = () => {
       <h3>Friends: </h3>
       {friends.map(friend =>
         <div key={friend.id}>{friend.username}
-          <Link to={`/friend/categories`}>
             <button onClick={() => clickView(friend.id)}>
               view
             </button>
-          </Link>
         </div>)}
     </FriendListDiv>
   )
