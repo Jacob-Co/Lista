a071704e025b0b96f2aa833d9c1bce9892643a41
Jacobtco <jacobtco@gmail.com>
2020-10-26 10:24:36 +0800

    Styled the div, gave a bottom border

--- frontend/src/components/WorkingOn.js
+++ frontend/src/components/WorkingOn.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const WorkingOnDiv = styled.div`
+  margin-right: 1rem;
+  padding-bottom: .5rem;
+  border-bottom: .15rem solid;
+`
 
 const WorkingOn = ({ category }) => {
   return(
     <Draggable draggableId={category.id} index={category.index}>
       {provided => 
-        <div
+        <WorkingOnDiv
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          {category.name}
-        </div>
+          <h2>Category currently working on:</h2>
+          <h3>{category.name}</h3>
+        </WorkingOnDiv>
       }
     </Draggable>
   );
