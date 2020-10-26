a69110321a0501c83abe7b3b51fc6761114ee8e5
Jacobtco <jacobtco@gmail.com>
2020-10-27 07:39:25 +0800

    Removed clickView, and now just uses Link to redirect page

--- frontend/src/components/FriendsList.js
+++ frontend/src/components/FriendsList.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { initializeFriends } from '../reducer/friendReducer';
-import { initializeFriendCategories } from '../reducer/friendCategoryReducer';
 
 const FriendListDiv = styled.div`
   position: absolute;
@@ -14,26 +13,20 @@ const FriendListDiv = styled.div`
 
 const FriendsList = () => {
   const dispatch = useDispatch();
-  const history = useHistory();
   const friends = useSelector(state => state.friends);
 
   useEffect(() => {
     dispatch(initializeFriends());
-  }, [dispatch])
-
-  const clickView = async (id) => {
-    await dispatch(initializeFriendCategories(id));
-    history.push('/friend/categories');
-  }
+  }, [dispatch]);
 
   return (
     <FriendListDiv>
       <h3>Friends: </h3>
       {friends.map(friend =>
         <div key={friend.id}>{friend.username}
-            <button onClick={() => clickView(friend.id)}>
-              view
-            </button>
+            <Link to={`/friend/categories/${friend.id}`}>
+              <button>view</button>
+            </Link>
         </div>)}
     </FriendListDiv>
   )
