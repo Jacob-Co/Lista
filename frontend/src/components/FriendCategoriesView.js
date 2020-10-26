748e9a0051abce4119e5d082e06ab4c6a01856cf
Jacobtco <jacobtco@gmail.com>
2020-10-27 07:55:36 +0800

    Placed useEffect to initializeFriendCategories

--- frontend/src/components/FriendCategoriesView.js
+++ frontend/src/components/FriendCategoriesView.js
@@ -1,15 +1,29 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { useParams } from 'react-router-dom';
+import { initializeFriendCategories } from '../reducer/friendCategoryReducer';
+
 const FriendCategoriesView = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
   const friendCategories = useSelector(state => state.friendCategories);
+
+  useEffect(() => {
+    dispatch(initializeFriendCategories(id));
+  }, [dispatch])
 
   return (
     <div>
       <h2>Currently Working On:</h2>
-      <p>{friendCategories[0].name}</p>
+      <p>{friendCategories.length > 0 
+        ? friendCategories[0].name
+        : 'Loading'}
+      </p>
       <h2>Other Categories</h2>
-      {friendCategories.slice(1).map(category => <p key={category.id}>{category.name}</p>)}
+      {friendCategories.length > 0 
+        ? friendCategories.slice(1).map(category => <p key={category.id}>{category.name}</p>)
+        : 'Loading'}
     </div>
   );
 };
