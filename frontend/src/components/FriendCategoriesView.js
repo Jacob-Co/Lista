8e432c971ad67e80436c7c7d0792fcb1d1ca0b21
Jacobtco <jacobtco@gmail.com>
2020-10-26 21:20:13 +0800

    Init FriendCategoriesView comp

---
+++ frontend/src/components/FriendCategoriesView.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { initializeFriendCategories } from '../reducer/friendCategoryReducer';
+
+const FriendCategoriesView = () => {
+  const dispatch = useDispatch();
+  const friendCategories = useSelector(state => state.friendCategories);
+
+  useEffect(() => {
+    dispatch(initializeFriendCategories());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2>Currently Working On:</h2>
+      <p>{friendCategories[0]}</p>
+      <h2>Other Categories</h2>
+      {friendCategories.slice(1).map(category => <p key={category.id}>{category.name}</p>)}
+    </div>
+  );
+};
+
+export default FriendCategoriesView;
