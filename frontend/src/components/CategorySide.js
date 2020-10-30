01363e5623ec63e0362498380db56d91a2c87921
Jacobtco <jacobtco@gmail.com>
2020-10-30 19:05:15 +0800

    Created a TaskList comp and tranfered all TaskList comp from CategorySide to there

--- frontend/src/components/CategorySide.js
+++ frontend/src/components/CategorySide.js
@@ -2,31 +2,13 @@ import React, { useRef } from 'react';
 import styled from "styled-components";
 import { Draggable } from 'react-beautiful-dnd';
 
-import TaskForm from './TaskForm'
-import Toggable from './Toggable'
-import Task from './Task';
-
-const TaskDiv = styled.div`
-  margin-left: 1.5rem;
-  margin-bottom: 1rem;
-`
+import TaskList from './TaskList'
 
 const DropDown = styled.div`
   margin-bottom: 1.5rem;
 `
 
 const CategorySide = ({category, deleteCategory, makeWorkingOn}) => {
-  const toggleTask = useRef();
-  const toggleCreateTask = useRef();
-
-  const showTasks = () => {
-    toggleTask.current.toggleVisibility(true);
-  }
-
-  const toggleTasks = () => {
-    toggleTask.current.toggleVisibility();
-  }
-
   return (
     <Draggable draggableId={category.id} index={category.index}>
       {provided => (
@@ -38,22 +20,8 @@ const CategorySide = ({category, deleteCategory, makeWorkingOn}) => {
         >
           <h3>
             <span onDoubleClick={() => makeWorkingOn(category.index)}>{category.name}</span>
-            <button onClick={() => {deleteCategory(category)}}>X</button>
-            <button onClick={() => toggleCreateTask.current.toggleVisibility()}>+</button>
-            { category.tasks.length > 0 ? <button onClick={toggleTasks}>&or;</button> : ''}
           </h3>
-          <Toggable ref={toggleCreateTask}>
-            <TaskDiv><TaskForm category={category} showTasks={showTasks}/></TaskDiv>
-          </Toggable>
-          <Toggable ref={toggleTask}>
-          { category.tasks.length > 0 
-            ? <TaskDiv className="tasks">
-                <h4>Tasks:</h4>
-                {category.tasks.map(task => <Task task={task} key={task.id} category={category}/>)}
-              </TaskDiv>
-            : ''
-          }
-          </Toggable>
+          <TaskList category={category} deleteCategory={deleteCategory}/>
         </DropDown>
       )}
     </Draggable>
---
+++ frontend/src/components/TaskList.js
@@ -0,0 +1,46 @@
+import React, { useRef } from 'react';
+import styled from 'styled-components';
+
+import TaskForm from './TaskForm';
+import Toggable from './Toggable';
+import Task from './Task';
+
+const TaskDiv = styled.div`
+  margin-left: 1.5rem;
+  margin-bottom: 1rem;
+`
+
+const TaskList = ({category, deleteCategory}) => {
+  const toggleTask = useRef();
+  const toggleCreateTask = useRef();
+
+  const showTasks = () => {
+    toggleTask.current.toggleVisibility(true);
+  }
+
+  const toggleTasks = () => {
+    toggleTask.current.toggleVisibility();
+  }
+
+  return (
+    <div>
+        <button onClick={() => {deleteCategory(category)}}>X</button>
+        <button onClick={() => toggleCreateTask.current.toggleVisibility()}>+</button>
+        { category.tasks.length > 0 ? <button onClick={toggleTasks}>&or;</button> : ''}
+        <Toggable ref={toggleCreateTask}>
+          <TaskDiv><TaskForm category={category} showTasks={showTasks}/></TaskDiv>
+        </Toggable>
+        <Toggable ref={toggleTask}>
+        { category.tasks.length > 0 
+          ? <TaskDiv className="tasks">
+              <h4>Tasks:</h4>
+              {category.tasks.map(task => <Task task={task} key={task.id} category={category}/>)}
+            </TaskDiv>
+          : ''
+        }
+        </Toggable>
+    </div>
+  )
+}
+
+export default TaskList;
