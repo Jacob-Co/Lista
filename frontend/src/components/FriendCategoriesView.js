4233657f50bfc42f59f9164b36dba84ab436321e
Jacobtco <jacobtco@gmail.com>
2020-11-02 19:02:15 +0800

    WorkingOn Category now shows tasks

--- frontend/src/components/FriendCategoriesView.js
+++ frontend/src/components/FriendCategoriesView.js
@@ -41,10 +41,15 @@ const FriendCategoriesView = () => {
     <FriendCategories>
       <h1>{friend ? capitalize(friend.username) : ""} Categories: </h1>
       <h2>Currently Working On:</h2>
-      <p><CategoryHeader>{friendCategories.length > 0 
-        ? friendCategories[0].name
+      <div><CategoryHeader>{friendCategories.length > 0 
+        ? <div>{friendCategories[0].name}
+            {friendCategories[0].tasks.length > 0
+              ? <FriendTasksView tasks={friendCategories[0].tasks}/>
+              : ""
+            }
+          </div>
         : 'Loading'}
-      </CategoryHeader></p>
+      </CategoryHeader></div>
       <h2>Other Categories:</h2>
       {friendCategories.length > 0 
         ? friendCategories.slice(1).map(category => <div key={category.id}>
