c30b667427a9350f7406b64e76911730c1ec118b
Jacobtco <jacobtco@gmail.com>
2020-11-03 11:38:25 +0800

    Added a Other Categories after WorkingOn comp

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -59,7 +59,10 @@ const CategoryList = () => {
             >
               {categoryList.map((category, position) => {
                 if (category === null || category === categoryList[0]) {
-                  return <WorkingOn key={category.id} category={category} categoryList={categoryList}/>
+                  return (<>
+                    <WorkingOn key={category.id} category={category} categoryList={categoryList}/>
+                    <h2>Other Categories:</h2>
+                  </>)
                 }
                 return <CategorySide 
                   className="category"
