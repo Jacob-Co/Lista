09c7ecb979b307cdf45d736a52d6ca66dca2aa5b
Jacobtco <jacobtco@gmail.com>
2020-11-04 18:56:05 +0800

    switchTaskWorkingOn now call uodateTaskWorkingOn to send new info to db

--- frontend/src/reducer/categoryReducer.js
+++ frontend/src/reducer/categoryReducer.js
@@ -182,6 +182,7 @@ export const switchTaskWorkingOn = (paramCategory, task, categoryList, categoryA
       type: 'UPDATE_CATEGORY',
       data: quickUpdatedCategoryList
     });
+    await categories.updateTaskWorkingOn(paramCategory.id, task.id);
     await updateCategoryIndexOnDb(quickUpdatedCategoryList);
   }
 }
