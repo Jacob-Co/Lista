a37ac5597947ab37bbad26a1b71621117f8e981c
Jacobtco <jacobtco@gmail.com>
2020-11-07 16:44:11 +0800

    Added a checked boolean prop that is used to either display a non checked or checked box

--- frontend/src/components/OptionBox.js
+++ frontend/src/components/OptionBox.js
@@ -3,12 +3,12 @@ import React, { useRef } from 'react';
 import Toggable from './Toggable';
 
 
-const OptionBox = ({ optionsArray }) => {
+const OptionBox = ({ optionsArray, checked }) => {
   const optionBoxRef = useRef();
 
   return(
     <Toggable ref={optionBoxRef}>
-
+      { checked ? <>&#9745;</> : <>&#9744;</>}
     </Toggable>
   )
 }
