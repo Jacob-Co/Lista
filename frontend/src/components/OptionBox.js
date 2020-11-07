bcc5d83b6877a7360f407bb077177ea870243d4b
Jacobtco <jacobtco@gmail.com>
2020-11-07 16:36:35 +0800

    Added a toggable and assigned a useRef to it

--- frontend/src/components/OptionBox.js
+++ frontend/src/components/OptionBox.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+import Toggable from './Toggable';
 
 
 const OptionBox = ({ optionsArray }) => {
+  const optionBoxRef = useRef();
 
+  return(
+    <Toggable ref={optionBoxRef}>
+
+    </Toggable>
+  )
 }
 
 export default OptionBox;
