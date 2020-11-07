e58a78493463b90776c9f9cb395cb2789ae2bd2a
Jacobtco <jacobtco@gmail.com>
2020-11-07 17:29:11 +0800

    Styled OptionBox and OptionDiv

--- frontend/src/components/OptionBox.js
+++ frontend/src/components/OptionBox.js
@@ -7,22 +7,31 @@ const CheckBox = styled.span`
   font-size: 1.2em;
 `
 const OptionBoxContent = styled.div`
+  position: absolute;
+  left: 1em;
+  padding: .35em;
 
+  background-color: black;
+  color: white;
+`
+
+const OptionDiv = styled.div`
+  margin: .35em 0;
 `
 
 const OptionBox = ({ optionsArray = [], checked }) => {
   const optionBoxRef = useRef();
 
   return(
-    <div>
+    <div style={{"position": "relative"}}>
       <div onClick={() => optionBoxRef.current.toggleVisibility()}>
         { checked ? <CheckBox>&#9745;</CheckBox> : <CheckBox>&#9744;</CheckBox>}
       </div>
       <Toggable ref={optionBoxRef}>
         <OptionBoxContent>
-          {optionsArray.map((option, idx) => <div key={idx}>
-            {option[0]}
-          </div>)}
+          {optionsArray.map((option, idx) => <OptionDiv key={idx}>
+            &#x000B7; {option[0]}
+          </OptionDiv>)}
         </OptionBoxContent>
       </Toggable>
     </div>
