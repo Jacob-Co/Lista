1637117473a319f0013312da63ac308f3bd93115
Jacobtco <jacobtco@gmail.com>
2020-11-09 09:42:20 +0800

    Added useState name

--- frontend/src/components/CategoryNameEditForm.js
+++ frontend/src/components/CategoryNameEditForm.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { patchCategoryName } from '../reducer/categoryReducer';
 
 const CategoryNameEditForm = ({ category, categoryList }) => {
+  const [ name, setName] = useState(category.name);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+  }
+
   return(
-    <form>
+    <form onsubmit={ handleSubmit }>
       <input
-        value={category.name}
+        value={name}
       />
       <button>edit</button>
     </form>
