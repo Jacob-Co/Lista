0d9d97df4becea4b47bc0177d0d848ba9490915f
Jacobtco <jacobtco@gmail.com>
2020-11-09 22:38:41 +0800

    Initialized io, passing server in

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const http = require('http');
-
 const app = require('./app');
+
 const config = require('./utils/config')
 
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 server.listen(config.PORT, () => {
   console.log(`Listening on PORT ${config.PORT}`)
