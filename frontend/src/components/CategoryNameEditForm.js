19a897faff8e7d85cedb8625e20085029fe5e6aa
Jacobtco <jacobtco@gmail.com>
2020-11-09 09:45:54 +0800

    Added a handleSumbit with dispatch to patchCategoryName

--- frontend/src/components/CategoryNameEditForm.js
+++ frontend/src/components/CategoryNameEditForm.js
@@ -3,18 +3,23 @@ import { useDispatch } from 'react-redux';
 
 import { patchCategoryName } from '../reducer/categoryReducer';
 
-const CategoryNameEditForm = ({ category, categoryList }) => {
+const CategoryNameEditForm = ({ category, categoryList, toggleEditing }) => {
   const [ name, setName] = useState(category.name);
+  const dispatch = useDispatch();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    if (name) {
+      dispatch(patchCategoryName(category.id, categoryList, name));
+      toggleEditing();
+    }
   }
 
   return(
     <form onsubmit={ handleSubmit }>
       <input
         value={name}
+        onChange={({target}) => setName(target.value)}
       />
       <button>edit</button>
     </form>
