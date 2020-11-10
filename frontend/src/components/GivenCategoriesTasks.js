6f0003c89b6471f12cc5e1afdf10482e41f8148b
Jacobtco <jacobtco@gmail.com>
2020-11-10 21:15:19 +0800

    Now displayed sentToItems

--- frontend/src/components/GivenCategoriesTasks.js
+++ frontend/src/components/GivenCategoriesTasks.js
@@ -1,8 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 import io from 'socket.io-client';
 
 import { getSentToItems } from '../reducer/sentToItemReducer';
+
+const ItemName = styled.span`
+  margin-left: 1.5rem;
+  font-size: 1.07em;
+  font-weight: bold;
+  text-decoration-line: ${props => (props.isAccomplished ? 'line-through;' : 'none;')}
+`
 
 const GivenCategoriesTasks = () => {
   const dispatch = useDispatch();
@@ -18,6 +26,12 @@ const GivenCategoriesTasks = () => {
   return(
     <div>
       <h2>{'Given Categories & Tasks'}</h2>
+      {sentToItems.map(item => <ItemName
+          key={item.id}
+        >
+        {item.name}
+        </ItemName>
+      )}
     </div>
   )
 }
