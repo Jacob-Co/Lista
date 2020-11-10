555b8259fad65810a66a91662681c5974cae2a17
Jacobtco <jacobtco@gmail.com>
2020-11-10 21:03:48 +0800

    Added a getSentToItems action reducer

--- frontend/src/reducer/sentToItemReducer.js
+++ frontend/src/reducer/sentToItemReducer.js
@@ -1,5 +1,15 @@
 import categories from '../services/categories';
 
+export const getSentToItems = () => {
+  return async (dispatch) => {
+    const sentToItems = await categories.getSentToItems();
+    dispatch({
+      type: 'INIT_SENT_TO_ITEMS',
+      data: sentToItems
+    })
+  }
+}
+
 const sentToItemReducer = (state = [], action) => {
   switch (action.type) {
     case 'INIT_SENT_TO_ITEMS':
