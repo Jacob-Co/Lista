334d82166979025b24af5b775f0b09be15ef338b
Jacobtco <jacobtco@gmail.com>
2020-11-10 22:06:02 +0800

    When clicking outside, hideSendForm

--- frontend/src/components/SendForm.js
+++ frontend/src/components/SendForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
@@ -6,15 +6,35 @@ const ButtonHolder = styled.div`
   margin-left: 1.5rem;
 `
 
-const SendForm = () => {
+const useOutsideEventListener = (ref, callback) => {
+  useEffect(() => {
+    const clickedOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+      }
+    }
+
+    document.addEventListener('mousedown', clickedOutside);
+    return () => {
+      document.removeEventListener('mousedown', clickedOutside);
+    }
+  }, [ref])
+};
+
+const SendForm = ({ hideSendForm }) => {
   const friends = useSelector(state => state.friends);
+  const sendFormRef = useRef();
+
+  useOutsideEventListener(sendFormRef, hideSendForm);
 
   return(
-    <div>
-      <h2>Sending to:</h2>
+    <div ref={sendFormRef}>
+      <h3>Sending to:</h3>
       <ButtonHolder>
-        {friends.map(friend => <button>
-          {friend.name}
+        {friends.map(friend => <button
+          key={friend.id}
+        >
+          {friend.username}
         </button>)}
       </ButtonHolder>
     </div>
