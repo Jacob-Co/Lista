c436f82b91c1c0108dc4b0a470657489d0949d1a
Jacobtco <jacobtco@gmail.com>
2020-11-10 18:37:48 +0800

    Added genericPatchHelper to patch '/accomplished/:id'

--- backend/controllers/categories.js
+++ backend/controllers/categories.js
@@ -173,19 +173,7 @@ categoryRouter.patch('/taskWorkingOn/:id', async (req, res) => {
 })
 
 categoryRouter.patch('/accomplished/:id', async (req, res) => {
-  const { token } = req;
-  if (!token) return res.status(401).json({ error: "Requires a token"});
-
-  const { body } = req;
-
-  const categoryToUpdate = await Category.findById(req.params.id);
-  if (!categoryToUpdate) return res.status(400).json({ "error": "No Category found"});
-  if (categoryToUpdate.user.toString() !== token.id) return res.status(401).json({ "error": "Invalid access" });
-
-  categoryToUpdate.accomplished = body.accomplished;
-  const returnCategory = await categoryToUpdate.save();
-
-  return res.json(returnCategory);
+  return await genericPatchHelper('accomplished', req, res);
 });
 
 categoryRouter.patch('/sentTo/:id', async (req, res) => {
