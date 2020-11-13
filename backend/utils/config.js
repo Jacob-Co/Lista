4f0e918cdd8fb05a62a683afa6d3c88fc6d66a6c
Jacobtco <jacobtco@gmail.com>
2020-11-13 17:13:44 +0800

    Uses a different DB when the NODE_ENV is equal to 'dev'

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 const { PORT } = process.env;
-const { MONGODB_URI } = process.env;
+const mongoDB_URI = process.env.NODE_ENV === 'dev' ? process.env.MONGODB_URI_DEV : process.env.MONGODB_URI;
 
-module.exports = { PORT, MONGODB_URI };
+module.exports = { PORT, mongoDB_URI };
