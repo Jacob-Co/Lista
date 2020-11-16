e8025dcbc58efe89494364f9666f017e38b6e228
Jacobtco <jacobtco@gmail.com>
2020-11-16 17:52:48 +0800

    Added a generateRandomCode and get requestStream for added security

--- backend/controllers/serverSideEvents.js
+++ backend/controllers/serverSideEvents.js
@@ -4,12 +4,47 @@ const User = require('../models/users');
 
 // Store open connections here
 const userIds = {};
+let accessCodes = [];
 
-serverSERouter.get('/stream', async (req, res) => {
+const generateRandomCode = () => {
+  const randomCodes = [
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+  ]
+
+  let code = '';
+  for ( let i = 0; i < 8; i++) {
+    const rand1 = Math.floor(Math.random() * 3);
+    const rand2 = Math.floor(Math.random() * 10);
+    code += randomCodes[rand1][rand2];
+  }
+
+  return code;
+}
+
+serverSERouter.get('/requestStream', async (req, res) => {
   const token = req;
-  if (!token) return res.status(400).json({ error: "requirs token" });
+  if (!token) return res.status(400).json({ error: "requires token" });
   const user = User.findById(token.id);
   if (!user) return res.status(400).json({ error: 'user not found' });
+
+  const code = generateRandomCode()
+  accessCodes = accessCodes.concat(code)
+
+  return res.status(200).json({ success: 'Server side connection established', code });
+})
+
+serverSERouter.get('/stream/:code', (req, res) => {
+  const code = req.params.code;
+  let isCorrectCode = false;
+  accessCodes = accessCodes.filter(accessCode => {
+    if (accessCode !== code) return true;
+    isCorrectCode = true;
+    return false
+  });
+
+  if (!isCorrectCode) return res.status(400).json({ error: 'wrong code'});
 
   res.setHeader('Content-Type', 'text/event-stream');
   res.socket.on('end', e => {
