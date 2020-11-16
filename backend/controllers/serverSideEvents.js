2259f990d223478bc251686e8d7934db90c80507
Jacobtco <jacobtco@gmail.com>
2020-11-16 17:10:14 +0800

    Added a serverSideEvents controller to send server side events to users

---
+++ backend/controllers/serverSideEvents.js
@@ -0,0 +1,23 @@
+const serverSERouter = require('express').Router();
+
+const User = require('../models/users');
+
+// Store open connections here
+const userIds = {};
+
+serverSERouter.get('/stream', async (req, res) => {
+  const token = req;
+  if (!token) return res.status(400).json({ error: "requirs token" });
+  const user = User.findById(token.id);
+  if (!user) return res.status(400).json({ error: 'user not found' });
+
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.socket.on('end', e => {
+    delete userIds[user.id]
+    console.log(`Remaining: ${Object.keys(userIds)}`)
+  });
+  userIds[user.id] = res;
+  console.log(Object.keys(userIds))
+})
+
+module.exports = serverSERouter;
