d46502bea0db78bc62efb44633e4ee167e4a0f96
Jacobtco <jacobtco@gmail.com>
2020-11-16 17:55:15 +0800

    establishSSE now calls get  to get code then get a new EventSource

--- frontend/src/services/serverSideEvents.js
+++ frontend/src/services/serverSideEvents.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 const baseUrl = '/serverSide';
 
+let sse;
+
 const establishSSE = async () => {
   const config = { headers: { Authorization: getToken() }};
-  const res = await axios.get(`${baseUrl}/stream`, config);
-  return res.data;
+  const res = await axios.get(`${baseUrl}/requestStream`, config);
+  
+  if (res.data.error) return res.data;
+
+  sse = new EventSource(`${baseUrl}/stream/${res.data.code}`);
 };
 
   export default { establishSSE };
