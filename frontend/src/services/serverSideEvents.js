a51ff813974fc1d88f70b65c15b68a6502bb8f4c
Jacobtco <jacobtco@gmail.com>
2020-11-17 17:46:17 +0800

    Placed SSE inside here, and added a new service estbalishSSE

--- frontend/src/services/serverSideEvents.js
+++ frontend/src/services/serverSideEvents.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 import { getToken } from './tokenHolder'
 
 const baseUrl = '/serverSide';
+let SSE;
 
-const getStreamCode = async (username) => {
+const getStreamCode = async () => {
   const config = { headers: { Authorization: getToken() }};
   const res = await axios.get(`${baseUrl}/requestStream`, config);
   
@@ -12,8 +13,8 @@ const getStreamCode = async (username) => {
   return res.data.code;
 };
 
-// const getSSE = (code, user) => {
-//   return new EventSource(`${baseUrl}/stream/${}`)
-// }
+const establishSSE = (code, username) => {
+  return new EventSource(`${baseUrl}/stream/${code}/${username}`)
+}
 
-  export default { getStreamCode };
+export default { getStreamCode, establishSSE };
