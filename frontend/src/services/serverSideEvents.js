0cd031a67f27310d0be2076be43d54721d8ce0e6
Jacobtco <jacobtco@gmail.com>
2020-11-17 09:29:02 +0800

    Removed establish SSE, and placed getStreamCode instead

--- frontend/src/services/serverSideEvents.js
+++ frontend/src/services/serverSideEvents.js
@@ -4,15 +4,12 @@ import { getToken } from './tokenHolder'
 
 const baseUrl = '/serverSide';
 
-let sse;
-
-const establishSSE = async (username) => {
+const getStreamCode = async (username) => {
   const config = { headers: { Authorization: getToken() }};
   const res = await axios.get(`${baseUrl}/requestStream`, config);
   
   if (res.data.error) return res.data;
-
-  sse = new EventSource(`${baseUrl}/stream/${res.data.code}/${username}`);
+  return res.data.code;
 };
 
-  export default { establishSSE };
+  export default { getStreamCode };
