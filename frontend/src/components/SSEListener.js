5e7158c935415b1659844812c1c85dbc00582fff
Jacobtco <jacobtco@gmail.com>
2020-11-18 11:31:18 +0800

    If offline or sse is closed, close connectoin then reconnect using connectToSSE

--- frontend/src/components/SSEListener.js
+++ frontend/src/components/SSEListener.js
@@ -1,31 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 
 import serverSideEvents from '../services/serverSideEvents'
 import { initializeCategories } from '../reducer/categoryReducer';
 
-const useEventSource = (url) => {
-  useEffect(() => {
-      const source = new EventSource(url);
+const useInterval = (callback, miliSeconds) => {
+  const savedCallback = useRef();
 
-      source.onmessage = function logEvents(event) {      
-          // updateData(JSON.parse(event.data));    
-          console.log(`message`) 
-      }
-  }, [])
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback])
+
+  useEffect(() => {
+    if (miliSeconds !== null) {
+      let intervalId = setInterval(savedCallback.current, miliSeconds);
+      return () => clearInterval(intervalId);
+    }
+  }, [miliSeconds])
 }
 
 const SSEListener =  ({ username }) => {
   const dispatch = useDispatch();
-
-  serverSideEvents.getStreamCode()
+  let sse;
+  const connectToSSE = () => {
+    serverSideEvents.getStreamCode()
     .then(code => {
-      const sse = serverSideEvents.establishSSE(code, username);
+      sse = serverSideEvents.establishSSE(code, username);
       sse.onmessage = e => {
         console.log(`hello`)
         dispatch(initializeCategories());
       }
     })
+  }
+
+  connectToSSE();
+
+  useInterval(() => {
+    if (sse.readyState === 2 || !navigator.onLine) {
+      sse.close();
+      connectToSSE();
+    };
+  }, 1000);
 
   return(<></>)
 }
