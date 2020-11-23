0398543415cd4b7a31eab94f8073e911543dfc5d
Jacobtco <jacobtco@gmail.com>
2020-11-23 17:58:52 +0800

    Added UniversalEditForm comp

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -3,9 +3,12 @@ import { useDispatch } from 'react-redux';
 import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
-import { removeTask } from '../reducer/categoryReducer';
-import { patchAccomplishedTask } from '../reducer/categoryReducer';
 import OptionBox from './OptionBox';
+import UniversalEditForm from './UniversalEditForm';
+//reducers
+import { removeTask, patchTaskAccomplished, patchTaskName } from '../reducer/categoryReducer';
+
+
 
 const TaskNameSpan = styled.span`
   text-decoration-line: ${props => (props.isAccomplished ? 'line-through' : 'none')}
@@ -25,6 +28,10 @@ const Task = ({ task, category, taskArrayIndex, makeTaskWorkingOn, categoryArray
     setIsEditing(!isEditing);
   }
 
+  const newTaskNameReducer = (newName) => {
+    return patchTaskName(task.id, newName)
+  }
+
   return(
     <Draggable draggableId={task.id} index={taskArrayIndex}>
       {provided => (
@@ -35,21 +42,28 @@ const Task = ({ task, category, taskArrayIndex, makeTaskWorkingOn, categoryArray
           <div style={{"display": "flex", "alignItems": "center"}}>
             <OptionBox optionsArray={
               [
-                ['Toggle Done', () => dispatch(patchAccomplishedTask(task.id, !task.accomplished))],
+                ['Toggle Done', () => dispatch(patchTaskAccomplished(task.id, !task.accomplished))],
                 ['Edit', toggleEditing],
                 ['Delete', handleDeleteTask]
               ]
             }/>
-            <TaskNameSpan 
-              isAccomplished={task.accomplished}
-              onDoubleClick={() => {
-                if (task.accomplished) return alert('Cannot work on accomplished task');
-                makeTaskWorkingOn(category, task, categoryArrayIndex) 
-              }}
-              {...provided.dragHandleProps}
-            >
-              {task.name}
-            </TaskNameSpan>
+            { isEditing 
+              ? <UniversalEditForm
+                  orignalValue={task.name}
+                  toggleEditing={toggleEditing}
+                  newValueReducer={newTaskNameReducer}
+                />
+              : <TaskNameSpan 
+                  isAccomplished={task.accomplished}
+                  onDoubleClick={() => {
+                    if (task.accomplished) return alert('Cannot work on accomplished task');
+                    makeTaskWorkingOn(category, task, categoryArrayIndex) 
+                  }}
+                  {...provided.dragHandleProps}
+                >
+                  {task.name}
+                </TaskNameSpan>
+            }
           </div>
         </div>
       )}
