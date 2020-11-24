5bf7064857026dd41f8149890eb9116e2d76a5b2
Jacobtco <jacobtco@gmail.com>
2020-11-24 15:02:19 +0800

    Removed taskForm

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -2,7 +2,6 @@ import React, { useRef } from 'react';
 import styled from 'styled-components'
 
 import Toggable from './Toggable';
-import TaskForm from './TaskForm';
 
 const TasksDiv = styled.div`
   margin-left: 1.5rem;
@@ -12,34 +11,19 @@ const AccomplishedSpan = styled.span`
   text-decoration-line: ${props => (props.isAccomplished ? 'line-through' : 'none')}
 `
 
-const TaskList = ({tasks, category, children}) => {
+const TaskList = ({tasks, children}) => {
   const taskToggableRef = useRef();
-  const taskFormToggableRef = useRef();
-  const showTaskFormRef = useRef();
-
-  const handleToggleTaskForm = () => {
-    taskFormToggableRef.current.toggleVisibility();
-    showTaskFormRef.current.focusOnName();
-  };
-
-  const showTasks = () => {
-    taskToggableRef.current.toggleVisibility(true)
-  }
 
   const toggleTasks = () => {
-    if (taskToggableRef.current.visible) taskFormToggableRef.current.toggleVisibility(false);
+    console.log(`toggleTasks`)
     taskToggableRef.current.toggleVisibility()
   }
 
   return(
     <>
-      {/* <button onClick={handleToggleTaskForm}>+</button> */}
       <button onClick={toggleTasks}>&or;</button>
       {children}
       <TasksDiv>
-        <Toggable ref={taskFormToggableRef}>
-          <TaskForm category={category} ref={showTaskFormRef} showTasks={showTasks}/>
-        </Toggable>
         <Toggable ref={taskToggableRef}>
           <h4>Tasks:</h4>
           {tasks.map(task => <div key={task.id}>
