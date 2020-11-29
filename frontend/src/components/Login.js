750ccb8dafb229d10882f325cb37bfa16d9f90bf
Jacobtco <jacobtco@gmail.com>
2020-11-29 16:48:17 +0800

    Added styled components

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { login } from '../reducer/tokenReducer'
-import { useHistory } from 'react-router-dom'
+import { login } from '../reducer/tokenReducer';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLoginDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 22%;
+`
+
+const ListaHeader = styled.h1`
+  text-align: center;
+`
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const StyledInputDiv = styled.div`
+  margin-bottom: 1rem;
+`
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -17,28 +39,32 @@ const Login = () => {
   }
 
   return (
-    <div>
-      <h1>Login to Ticket Book</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          Username: 
-          <input
-            type="text"
-            name="username"
-            autoComplete="username"
-          />
-        </div>
-        <div>
-          Password:
-          <input
-            type="password"
-            name="password"
-            autoComplete="password"
-          />
-        </div>
-        <button>Login</button>
-      </form>
-    </div>
+    <StyledLoginDiv>
+      <div>
+        <ListaHeader>Lista</ListaHeader>
+        <StyledForm onSubmit={handleLogin}>
+          <div>
+            <StyledInputDiv>
+              Username: 
+              <input
+                type="text"
+                name="username"
+                autoComplete="username"
+              />
+            </StyledInputDiv>
+            <StyledInputDiv>
+              Password:
+              <input
+                type="password"
+                name="password"
+                autoComplete="password"
+              />
+            </StyledInputDiv>
+          </div>
+          <button>Login</button>
+        </StyledForm>
+      </div>
+    </StyledLoginDiv>
   )
 }
 
