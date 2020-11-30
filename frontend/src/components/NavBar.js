35000d50f92a746b218cf0955718bd2aad3e8a33
Jacobtco <jacobtco@gmail.com>
2020-12-01 07:10:28 +0800

    Added Logout on OptionBox's optionsArray

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
 import OptionBox from './OptionBox';
 import HomeClock from './HomeClock';
+import { closeSSEConnection } from '../reducer/sseReducer';
+import { logout } from '../reducer/tokenReducer';
 
 const NavBarDiv = styled.div`
   display: flex;
@@ -24,10 +28,20 @@ const LeftDiv = styled.div`
 `
 
 const NavBar = () => {
+const dispatch = useDispatch();
+const history = useHistory();
+
+const handleLogout = () => {
+  dispatch(closeSSEConnection());
+  dispatch(logout());
+  history.push('/');
+}
+
   return (
     <NavBarDiv>
       <RightDiv>
         <OptionBox
+          optionsArray={[['Logout', handleLogout]]}
           icon={<>&#x2630;</>}
         />
         <ListaHeader>Lista</ListaHeader>
