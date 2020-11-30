13e61f44ce383f72a6480c8507bed79ef27b8c30
Jacobtco <jacobtco@gmail.com>
2020-12-01 06:50:10 +0800

    Added HomeClock

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import OptionBox from './OptionBox';
+import HomeClock from './HomeClock';
+
 const NavBarDiv = styled.div`
-  position: fixed;
-  top: 0;
-  margin-bottom: 2rem;
+  display: flex;
+  align-items: center;
+  margin-top: 1rem;
+  padding-left: .45rem;
+  justify-content: space-between
+`
+const ListaHeader = styled.h1`
+  margin: 0;
+  margin-left: .45rem;
+`
+const RightDiv = styled.div`
+  display: flex;
+  align-items: center;
+`
+const LeftDiv = styled.div`
+  margin-right: .55rem;
 `
 
 const NavBar = () => {
   return (
     <NavBarDiv>
-      <h1>Lista</h1>
+      <RightDiv>
+        <OptionBox
+          icon={<>&#x2630;</>}
+        />
+        <ListaHeader>Lista</ListaHeader>
+      </RightDiv>
+      <LeftDiv>
+        <HomeClock />
+      </LeftDiv>
     </NavBarDiv>
   )
 }
