f55c7e1a6912f674a6f9f341d508bca4121f91bc
Jacobtco <jacobtco@gmail.com>
2020-12-05 18:05:03 +0800

    Added a test draggable list

--- frontend/src/components/FAQ.js
+++ frontend/src/components/FAQ.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 
 const FAQ = () => {
+  const [items, setItems] = useState(['item1', 'item2', 'item3', 'item4']);
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+  
+    return result;
+  };
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+
+    if (result.destination.index === result.source.index) {
+      return;
+    }
+
+    const quotes = reorder(
+      state.quotes,
+      result.source.index,
+      result.destination.index
+    );
+
+    setState({ quotes });
+  }
+
   return(
     <div>
-      <h1>FAQ</h1>
+      <h2>FAQ:</h2>
+      <div>
+        <h3>You can rearrange projects or tasks by dragging their names! Test Below:</h3>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="list">
+            {provided => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {items.map((item, index) => {
+                  return(
+                    <Draggable draggableId={index} index={index}>
+                      {provided => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          {item}
+                        </div>
+                      )}
+                    </Draggable>
+                  )
+                })}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     </div>
   )
 }
